b16084c171a420cdc4e8a0bf17e267d5c8489fe7
Victor Vecchio <111601821+victor-rva@users.noreply.github.com>
2023-09-27 11:18:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from Desktop import Desktop
 from Notebook import Notebook
 
+"""
+Construir código necessário em Python para implementar o seguinte projeto: Uma classe abstrata chamada de Computador que contém os atributos/propriedades: modelo, cor e preço. Esta classe possui um método getInformacoes() que retorna todos os valores dos atributos. Esta classe também possui um método abstrato cadastrar().
+ 
+O projeto também deve possuir as classes Desktop e Notebook que herdam da classe Computador. A classe Desktop possui o atributo/propriedade fracamente privado potenciaDaFonte. Esta classe reimplementa o método getInformacoes() herdado de computador.
+ 
+A classe Notebook possui o atributo/propriedade fortemente privado tempoDeBateria. Esta classe reimplementa o método getInformacoes() herdado de computador.
+ 
+Construa um arquivo do tipo main com a utilização das classes construídas, para teste dos algoritmos.
+"""
+
 d1 = Desktop("Lenovo", "vermelho", 2400, 1250)
 n1 = Notebook("Apple", "preto", 3000, 12)
 
