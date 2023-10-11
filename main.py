f2def4f225b84e5cf3b6d9c5264f744732a60601
Victor Vecchio <111601821+victor-rva@users.noreply.github.com>
2023-10-11 11:15:32 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from Lista import Lista
+
+lista = Lista()
+
+lista.imprimir()
+lista.addNoInicio(10)
+lista.addNoInicio(5)
+lista.addNoFim(20)
+
+lista.removerDoInicio()
+lista.removerDoFim()
+lista.removerDoFim()
