257dc216b86633db91c3a7eece2c5baced2f67ad
Victor Vecchio <victorvecchio.rva@gmail.com>
2023-10-18 09:54:59 -0300

    exercicio Aula09

---
+++ Aula09-ListaEncadeada/exercicio/main.py
@@ -0,0 +1,16 @@
+from Lista import Lista
+
+lista = Lista()
+
+lista.imprimir()
+lista.add( 14 )
+lista.add( 10 )
+lista.add(  8 )
+lista.add( 15 )
+lista.add( 12 )
+
+lista.remover( 12 )
+lista.remover( 7 )
+lista.remover( 15 )
+lista.remover( 10 )
+lista.remover( 14 )
