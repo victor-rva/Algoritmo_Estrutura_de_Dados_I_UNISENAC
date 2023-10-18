2eb7076339d9f719ba864f8363f8b3b25d82e891
Victor Vecchio <victorvecchio.rva@gmail.com>
2023-10-18 11:00:28 -0300

    Termininar exercicio

--- Aula10-ListaDuplamenteEncadeada/Lista.py
+++ Aula10-ListaDuplamenteEncadeada/Lista.py
@@ -1,75 +1,99 @@
 from No import No
 
+# class Lista:
+#     def __init__(self):
+#         self.inicio = None
+#         self.tamanho = 0
+
+#     def addNoInicio(self, valor):
+#         no = No(valor)
+#         if self.inicio == None:
+#             self.inicio = no
+#             self.tamanho += 1
+#         else:
+#             no.proximo = self.inicio
+#             self.inicio = no
+#             self.tamanho += 1
+#         # self.imprimir()
+
 class Lista:
     def __init__(self):
         self.inicio = None
+        self.fim = None
         self.tamanho = 0
-        
-    def add(self, valor):
-        nodo = No(valor)
-        if self.inicio == None:
-            self.inicio = nodo
-        else:
-            if nodo.dado < self.inicio.dado:
-                nodo.proximo = self.inicio
-                self.inicio = nodo
-            else:
-                ant = self.inicio
-                aux = self.inicio.proximo
-                while aux != None :
-                    if nodo.dado < aux.dado:
-                        nodo.proximo = ant.proximo   
-                        ant.proximo = nodo
-                        #aux = nodo.proximo
-                        break
-                    else: 
-                        ant = aux
-                        aux = aux.proximo
-                if aux == None and nodo.dado >= ant.dado:
-                    ant.proximo = nodo
-                    
+
+    def addNoInicio(self, valor):
+        no = No(valor)
+        if self.inicio != None:
+            no.proximo = self.inicio
+        self.inicio = no
         self.tamanho += 1
         self.imprimir()
-                            
-    def imprimir(self):
-        print("---------------------")
+
+    def addNoFim(self, valor):
+        no = No(valor)
         if self.inicio == None:
-            print("Lista Vazia!")
+            self.inicio = no
+            self.fim = no
         else:
-            aux = self.inicio
-            while aux :
-                print( aux.dado )
-                aux = aux.proximo
-            print( "Tamanho: ", str(self.tamanho))
-            
-    def remover(self, valor):
-        tamAtual = self.tamanho
+            self.fim.proximo = no
+            self.anterior = self.fim
+            self.fim = no
+        self.tamanho += 1
+        self.imprimir()
+        
+    def imprimir(self):
         if self.inicio == None:
             print("Lista Vazia")
-            
-            # Lista contendo só um elemente 
-            # e este é igual ao valor
-        elif self.inicio.proximo == None and self.inicio.dado == valor:
+        else:
+            aux = self.inicio
+            while aux:
+                print(aux.dado)
+                aux = aux.proximo
+            print("Total: ", self.tamanho)
+
+    def imprimirReverso(self):
+        if self.inicio == None:
+            print("Lista Vazia")
+        else:
+            aux = self.fim
+            while aux:
+                print(aux.dado)
+                aux = aux.anterior
+            print("Total: ", self.tamanho)
+
+    # def removerDoInicio(self):
+    #     if self.inicio == None:
+    #         print("Lista Vazia")
+    #     elif self.inicio.proximo == None:
+    #         self.inicio = None
+    #         self.tamanho = 0
+    #     else:
+    #         self.inicio = self.inicio.proximo
+    #         self.tamanho -= 1
+    #     self.imprimir()
+    
+    def removerDoInicio(self):
+        if self.inicio == None:
+            print("Lista Vazia")
+        else:
+            self.inicio = self.inicio.proximo
+            self.tamanho -= 1
+        self.imprimir()
+
+    def removerDoFim(self):
+        if self.inicio == None:
+            print("Lista Vazia")
+        elif self.inicio.proximo == None:
             self.inicio = None
             self.tamanho = 0
         else:
-            # Lista contendo vários elementos e o valor é igual ao primeiro
-            if self.inicio.dado == valor:
-                self.inicio = self.inicio.proximo
-                self.tamanho -= 1
-            # Lista com vários elementos e o valor não está no primeiro    
-            else:
-                ant = self.inicio
-                aux = self.inicio.proximo
-                while aux :
-                    if aux.dado == valor:
-                        ant.proximo = aux.proximo
-                        self.tamanho -= 1
-                        break
-                    else:
-                        ant = aux
-                        aux = aux.proximo
-        if tamAtual == self.tamanho:
-            print("Valor não encontrado")
+            anterior = self.inicio
+            aux = self.inicio.proximo
+            while aux.proximo:
+                anterior = aux 
+                aux = aux.proximo
+            anterior.proximo = None
+            self.tamanho -= 1
         self.imprimir()
-    
+        self.imprimirReverso()
---
+++ Aula10-ListaDuplamenteEncadeada/main.py
@@ -0,0 +1,21 @@
+from Lista import Lista
+
+lista = Lista()
+
+lista.removerDoInicio()
+lista.removerDoFim()
+lista.addNoInicio( 10 )
+lista.addNoInicio(  8 )
+lista.addNoInicio(  12 )
+lista.addNoInicio(  9 )
+lista.addNoInicio(  3 )
+lista.addNoInicio(  2 )
+lista.addNoFim(  1 )
+lista.addNoFim(  14 )
+lista.addNoFim(  19 )
+lista.addNoFim(  20 )
+lista.addNoFim(  21 )
+lista.addNoFim(  13 )
+lista.removerDoInicio()
+lista.removerDoFim()
+lista.removerDoFim()
