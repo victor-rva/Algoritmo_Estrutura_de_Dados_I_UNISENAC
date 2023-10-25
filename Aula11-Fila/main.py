f849d9feed0a799c144d2c708bad3af6f3d277cf
Victor Vecchio <victorvecchio.rva@gmail.com>
2023-10-25 11:43:50 -0300

    Aula11-Fila/FIFO

---
+++ Aula11-Fila/main.py
@@ -0,0 +1,13 @@
+from Fila import Fila
+fila = Fila()
+
+fila.imprimir()
+fila.remover()
+fila.add("Victor")
+fila.add("Adalto")
+fila.add("Maria")
+fila.remover()
+fila.add("João")
+fila.remover()
+fila.remover()
+fila.remover()
