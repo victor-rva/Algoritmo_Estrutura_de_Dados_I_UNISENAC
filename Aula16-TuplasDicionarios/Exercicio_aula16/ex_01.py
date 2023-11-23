10e1da4bca0095e96214c957e1cf039eb0762d94
Victor Vecchio <victorvecchio.rva@gmail.com>
2023-11-23 00:56:31 -0300

    Realização dos exercicíos da aula 16

--- Aula16-TuplasDicionarios/Exercicio_aula16/ex_01.py
+++ Aula16-TuplasDicionarios/Exercicio_aula16/ex_01.py
@@ -2,3 +2,27 @@
 por extenso de “zero” a “nove”. Peça ao usuário para digitar um número
 de 0 a 9 e retorne a ele o número por extenso, sem usar estruturas
 condicionais (if e switch)"""
+
+numeros_extenso = ("zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove")
+
+def algarismo():
+    numero = int(input("Digite um número de 0 a 9: "))
+    return print(numeros_extenso[numero])
+
+algarismo()
+
+# try:
+#     numero_digitado = int(input('Digite um número de 0 a 9: '))
+    
+#     # Verifique se o número digitado está dentro do intervalo válido
+#     validade = 0 <= numero_digitado <= 9
+    
+#     # Use a lógica matemática para calcular o índice e acessar o elemento na tupla
+#     print(f'O número por extenso é: {numeros_extenso[validade * numero_digitado]}')
+# except ValueError:
+#     print('Por favor, digite um número válido.')
+    
+    # """
+    #  Os valores booleanos (True/False) podem ser tratados numericamente como 1 e 0. Portanto, usando essa expressão, você obtém diretamente o número digitado 
+    #  como índice quando ele está dentro do intervalo válido (de 0 a 9) e obtém 0 quando o número está fora desse intervalo.
+    # """
--- Aula16-TuplasDicionarios/Exercicio_aula16/ex_02.py
+++ Aula16-TuplasDicionarios/Exercicio_aula16/ex_02.py
@@ -5,3 +5,23 @@ dicionário. Após, calcule a nota final deste aluno [(nota01 + nota02) /2]
 e adicione ao dicionário. Ao final, imprima todos os dados do
 dicionário
 """
+
+notas = {
+    "nome": "",
+    "nota01": "",
+    "nota02": "",
+    "notaFinal" : ""
+}
+nome = input("Digite o nome do aluno: ")
+nota01 = int(input("Digite a nota da avaliação 01 do aluno: "))
+nota02 = int(input("Digite a nota da avaliação 02 do aluno: "))
+nota_final = (nota01 + nota02) /2
+print("Nota final: ", nota_final)
+print("------------------------")
+
+notas["nome"] = nome
+notas["nota01"] = nota01
+notas["nota02"] = nota02
+notas["notaFinal"] = nota_final
+
+print(notas)
